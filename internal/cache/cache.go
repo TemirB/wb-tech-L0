7c851b7d7d0300a6ad2087953b03a68b88c5ab6d
TemirB <balinov.tm21@physics.msu.ru>
2025-08-24 22:20:19 +0300

    cache refactor

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,34 +1,46 @@
 package cache
 
 import (
+	"context"
+
+	"github.com/TemirB/wb-tech-L0/internal/config"
 	"github.com/TemirB/wb-tech-L0/internal/domain"
 
 	lru "github.com/hashicorp/golang-lru/v2"
 )
 
-type LRU struct {
-	c *lru.Cache[string, domain.Order]
+type repo interface {
+	GetByUID(ctx context.Context, uid string) (*domain.Order, error)
+	RecentOrderIDs(ctx context.Context, limit int) ([]string, error)
 }
 
-func New(size int) (*LRU, error) {
+type Cache struct {
+	lru *lru.Cache[string, domain.Order]
+}
+
+func New(size int) (*Cache, error) {
 	c, err := lru.New[string, domain.Order](size)
 	if err != nil {
 		return nil, err
 	}
-	return &LRU{c}, nil
+	return &Cache{c}, nil
 }
 
-func (l *LRU) Get(uid string) (*domain.Order, bool) {
-	order, ok := l.c.Get(uid)
+func (c *Cache) Warm(ctx context.Context, cfg config.Config, repo repo) {
+	if ids, err := repo.RecentOrderIDs(ctx, cfg.CacheCap); err == nil {
+		for _, id := range ids {
+			if o, err := repo.GetByUID(ctx, id); err == nil {
+				c.Set(o)
+			}
+		}
+	}
+}
+
+func (c *Cache) Get(uid string) (*domain.Order, bool) {
+	order, ok := c.lru.Get(uid)
 	return &order, ok
 }
 
-func (l *LRU) Set(order *domain.Order) {
-	l.c.Add(order.OrderUID, *order)
-}
-
-func (c *LRU) Warm(list []*domain.Order) {
-	for _, o := range list {
-		c.Set(o)
-	}
+func (c *Cache) Set(order *domain.Order) {
+	c.lru.Add(order.OrderUID, *order)
 }
